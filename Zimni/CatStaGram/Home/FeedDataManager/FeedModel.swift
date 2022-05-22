b7396aab8854df594ff9b9f2b797a07e7e647e7d
yujimin413 <84549242+yujimin413@users.noreply.github.com>
2022-05-23 01:53:10 +0900

    Cat API - HomeFeed

---
+++ Zimni/CatStaGram/Home/FeedDataManager/FeedModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedModel.swift
+//  CatStaGram
+//
+//  Created by 유지민 on 2022/05/23.
+//
+
+struct FeedModel : Decodable {
+    var id : String?
+    var url : String?
+}
--- Zimni/CatStaGram/Home/HomeViewController.swift
+++ Zimni/CatStaGram/Home/HomeViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
+import Kingfisher
 
 class HomeViewController: UIViewController {
     
-    
-
     @IBOutlet weak var tableView: UITableView!
+    var arrayCat : [FeedModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +25,15 @@ class HomeViewController: UIViewController {
         tableView.register(storyNib, forCellReuseIdentifier: "StoryTableViewCell")
         
         // Do any additional setup after loading the view.
+        let input = FeedAPIInput(limit: 10, page: 0)
+        FeedDataManager().feedDataManager(input, self)
     }
 
 }
 
 extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return arrayCat.count + 1
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
@@ -44,7 +46,10 @@ extension HomeViewController : UITableViewDelegate, UITableViewDataSource {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell", for: indexPath) as? FeedTableViewCell else {
                 return UITableViewCell()
             }
-            cell.selectionStyle = .none
+            if let urlString = arrayCat[indexPath.row - 1].url {
+                let url = URL(string:urlString)
+                cell.imageViewFeed.kf.setImage(with: url)
+            }
             return cell
         }
     }
@@ -81,4 +86,11 @@ extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSou
         return CGSize(width: 50, height: 60)
     }
 
+}
+
+extension HomeViewController {
+    func successAPI(_ result : [FeedModel]) {
+        arrayCat = result
+        tableView.reloadData()
+    }
 }
