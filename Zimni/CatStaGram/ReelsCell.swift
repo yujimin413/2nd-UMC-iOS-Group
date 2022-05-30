e552ed834e47939d85ce46e5496f8a2f56b1b0ef
yujimin413 <84549242+yujimin413@users.noreply.github.com>
2022-05-30 09:19:30 +0900

    Reels - construct screen 2

--- Zimni/CatStaGram/ReelsCell.swift
+++ Zimni/CatStaGram/ReelsCell.swift
@@ -14,6 +14,21 @@ class ReelsCell: UICollectionViewCell {
     
     var videoView: VideoPlayerView?
     
+    // 릴스
+    let cellTitleLabel = UILabel()
+    
+    // 카메라
+    let cameraImageView = UIImageView()
+    
+    // 댓글
+    let commentImageView = UIImageView()
+    
+    // 좋아요
+    let likeImageView = UIImageView()
+    
+    // 공유하기
+    let shareImageView = UIImageView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -29,6 +44,11 @@ class ReelsCell: UICollectionViewCell {
     }
     
     private func setupAttribute() {
+        cellTitleLabel.text = "릴스"
+        cellTitleLabel.font = .boldSystemFont(ofSize: 25)
+        cellTitleLabel.textAlignment = .left
+        
+        cameraImageView.image = UIImage(systemName: "camera")
         
     }
     
@@ -38,6 +58,17 @@ class ReelsCell: UICollectionViewCell {
         
         videoView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+        }
+        
+        [cellTitleLabel, cameraImageView,
+        likeImageView,
+        commentImageView,
+        shareImageView]
+            .forEach { contentView.addSubview($0) }
+        
+        cellTitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalToSuperview().offset(20)
         }
     }
 }
